145f78041cd540772a6d06b46bc8246d249f6430
LizbethMayumy <mayumy19liss@gmail.com>
2023-10-10 21:02:05 -0500

    primer Commit

---
+++ app/Http/Controllers/estudianteControlador.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\estudiante;
+
+use Illuminate\Http\Request;
+
+use App\routes\web;
+
+
+class estudianteControlador extends Controller
+{
+    public function vista(){
+        return view('index');
+
+    }
+
+    public function create(Request $request){
+    
+        $estudiante= new estudiante();
+        
+        $estudiante->DNI=$request->DNI;
+        $estudiante->Nombre=$request->Nombre;
+        $estudiante->Apellido=$request->Apellido;
+        $estudiante->save();
+        return redirect('/create');
+
+    }
+    
+}
---
+++ app/Models/estudiante.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\estudiante;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class estudiante extends Model
+{
+    protected $table='estudiante';
+}
